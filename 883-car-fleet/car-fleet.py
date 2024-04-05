2013a6f28274943c0a643f2e33ab933553097431
sai_bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-04-04 21:03:45 -0500

    Time: 772 ms (18.25%) | Memory: 39.3 MB (27.37%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,14 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        if len(position) == 1:
-            return 1
-
         times = []
         for i in range(len(position)):
             times.append([position[i], ((target - position[i]) / speed[i])])
 
         times.sort()
-        # print(times)
         i = len(times) - 2
         ans = [times[-1][1]]
 
