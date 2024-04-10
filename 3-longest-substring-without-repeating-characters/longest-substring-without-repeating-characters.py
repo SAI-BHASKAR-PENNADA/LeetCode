b19ba6c3a8215af4f4f2bc2ce68a281f9ec6ea61
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-04-10 01:27:58 -0500

    Time: 59 ms (44.18%) | Memory: 16.7 MB (19.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,18 +1,42 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:
+        # if len(s) == 0:
+        #     return 0
+
+        # charmap = {}
+        # ans = 0
+        # start = 0
+        # for i in range(len(s)):
+        #     if s[i] not in charmap:
+        #         ans = max(ans, i - start + 1)
+        #     else:
+        #         index = charmap[s[i]]
+        #         if start <= index:
+        #             start = index + 1
+        #         ans = max(ans, i - start + 1)
+        #     charmap[s[i]] = i
+        # return ans
+
+        if not s:
             return 0
 
+        i = 0
+        j = 1
+        ans = 1
         charmap = {}
-        ans = 0
-        start = 0
-        for i in range(len(s)):
-            if s[i] not in charmap:
-                ans = max(ans, i - start + 1)
+        charmap[s[i]] = 0
+        while j < len(s):
+            if s[j] in charmap:
+                while i < j:
+                    if s[i] == s[j]:
+                        break
+                    else:
+                        del charmap[s[i]]
+                    i += 1
+                i += 1
+                j += 1
             else:
-                index = charmap[s[i]]
-                if start <= index:
-                    start = index + 1
-                ans = max(ans, i - start + 1)
-            charmap[s[i]] = i
+                ans = max(ans, j - i + 1)
+                charmap[s[j]] = 0
+                j += 1
         return ans
