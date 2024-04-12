1357fa31763b1f8fde696e165ec5db0c7a9491a4
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-04-12 16:40:41 -0500

    Time: 53 ms (88.42%) | Memory: 52 MB (10.65%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let i = 0;
-
-    while (i < nums.length) {
-       let j = i + 1;
-       while (j < nums.length) {
-            if (nums[i] + nums[j] == target)
-                return [i, j];
-            j = j + 1;
-       }
-        i = i + 1;
-    }
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let search = target - nums[i];
+        if (map.has(search)) {
+            return [map.get(search), i]
+        }
+        
+        if (!map.has(nums[i])) {
+            map.set(nums[i], i);
+        }
+    }   
 };
