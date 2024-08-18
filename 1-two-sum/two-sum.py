b684c8e89f4c76eb166c572e7ada0bf0340fb1f9
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-08-18 16:50:54 -0400

    Time: 57 ms (73.12%) | Memory: 17.7 MB (28.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # O(n^2)
+        # for i in range(0, len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        # O(n)
+        store = {}
         for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            lookfor = target - nums[i]
+            if lookfor in store:
+                return [i, store[lookfor]]
+            store[nums[i]] = i
+
