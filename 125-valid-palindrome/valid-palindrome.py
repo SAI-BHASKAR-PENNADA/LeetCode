ec2cedaf07e025d6267759ab124b9fdef7bd62e8
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-08-24 00:01:30 -0400

    Time: 55 ms (14.71%) | Memory: 17 MB (82.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,19 @@
 class Solution:
     def isalphanum(self, ch):
-        if (ch >= 65 and ch <= 90) or (ch >= 97 and ch <= 122) or (ch >= 48 and ch <= 57):
+        if (ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z') or (ch >= '0' and ch <= '9'):
             return True
         return False
 
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         i, j = 0, len(s) - 1
 
         while i < j:
-            if not self.isalphanum(ord(s[i])):
+            if not self.isalphanum(s[i]):
                 i += 1
-            elif not self.isalphanum(ord(s[j])):
+            elif not self.isalphanum(s[j]):
                 j -= 1
-            elif s[i].lower() == s[j].lower():
+            elif s[i] == s[j]:
                 i += 1
                 j -= 1
             else:
