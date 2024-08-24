54a292295e9d2b12195446e435a8ffa767cca4d6
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-08-24 00:21:53 -0400

    Time: 94 ms (38.34%) | Memory: 19.3 MB (94.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,40 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapList = []
-        for st in strs:
-            dictionary = {}
-            for ch in st:
-                if dictionary.get(ch) is None:
-                    dictionary[ch] = 1
-                else:
-                    dictionary[ch] = dictionary[ch] + 1 
-            mapList.append(dictionary)
-
-        answer = [[strs[0]]]
-        # while len(mapList) is not 0:
-        #     group = []
-        #     placeholder = []
-        #     phStrs = []
-        #     firstMap = mapList.pop(0)
-        #     firstStr = strs.pop(0)
-        #     group.append(firstStr)
-        #     for i in range(0, len(mapList)):
-        #         if len(firstStr) == len(strs[i]) and firstMap == mapList[i]:
-        #             group.append(strs[i])
-        #         else:
-        #             placeholder.append(mapList[i])
-        #             phStrs.append(strs[i])
-        #     answer.append(group)
-        #     mapList = placeholder
-        #     strs = phStrs
-        maps = [mapList[0]]
-
-        for i in range(1, len(mapList)):
-            if mapList[i] not in maps:
-                maps.append(mapList[i])
-                answer.append([strs[i]])
+        
+        mymap = {}
+        for string in strs:
+            sortedString = "".join(sorted(string))
+            # print(sortedString)
+            if sortedString in mymap:
+                mymap[sortedString].append(string)
             else:
-                index = maps.index(mapList[i])
-                answer[index].append(strs[i])
-
-        return answer
+                mymap[sortedString] = [string]
+        
+        ans = []
+        for maps in mymap:
+            ans.append(mymap[maps])
+        return ans
