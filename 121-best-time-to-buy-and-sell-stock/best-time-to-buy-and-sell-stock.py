b1d0e47dc8d5d2d928966f938864374e674bd300
sai bhaskar <123841411+SAI-BHASKAR-PENNADA@users.noreply.github.com>
2024-09-27 14:52:44 -0400

    Time: 836 ms (5.12%) | Memory: 27.5 MB (12.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        maxarr = [0, prices[-1]]
+        for i in range(len(prices) - 3, -1, -1):
+            maxarr.append(max(maxarr[-1], prices[i+1]))
+        maxarr.reverse()
+
         ans = 0
-
-        bestprice = [0]*len(prices)
-        bestprice[-1] = prices[-1]
-        for i in range(len(prices) - 2, -1, -1):
-            bestprice[i] = max(prices[i], bestprice[i+1])
-
-        
-        for i in range(len(prices) - 1):
-            ans = max(ans, bestprice[i+1] - prices[i])
-        return ans
+        for i in range(0, len(prices)):
+            profit = maxarr[i] - prices[i]
+            ans = max(ans, profit)
+        return ans 
